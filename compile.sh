3fc90f8433bf60344c0e25b92662375f57eb5032
Andreica Vlad <andreicavlad2@gmail.com>
2026-08-31 14:17:31 +0300

    Compiles code to binary

--- compile.sh
+++ compile.sh
@@ -3,3 +3,4 @@ g++ divizori.cpp -o divizori
 g++ extragerea_radicalilor.cpp -o extragerea_radicalilor
 g++ heron.cpp -o heron
 g++ simplif_fractii.cpp -o simplif_fractii
+g++ prime_factors.cpp -o prime_factors
