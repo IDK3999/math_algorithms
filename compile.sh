dac6cd9332334398667c441fa6d10362f8bf239f
Andreica Vlad <andreicavlad2@gmail.com>
2026-09-02 17:55:58 +0300

    Compiles C++ code to binary

--- compile.sh
+++ compile.sh
@@ -4,3 +4,4 @@ g++ extragerea_radicalilor.cpp -o extragerea_radicalilor
 g++ heron.cpp -o heron
 g++ simplif_fractii.cpp -o simplif_fractii
 g++ prime_factors.cpp -o prime_factors
+g++ teorema_cosinusului.cpp -o teorema_cosinusului
