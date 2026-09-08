42bf30b693adecdb4a4c618e2bafb8861c89f89f
Andreica Vlad <andreicavlad2@gmail.com>
2026-09-08 19:09:33 +0300

    Compiles C++ to binary

--- compile.sh
+++ compile.sh
@@ -1,7 +1,9 @@
-g++ cmmdc.cpp -o cmmdc
-g++ divizori.cpp -o divizori
-g++ extragerea_radicalilor.cpp -o extragerea_radicalilor
-g++ heron.cpp -o heron
-g++ simplif_fractii.cpp -o simplif_fractii
-g++ prime_factors.cpp -o prime_factors
-g++ teorema_cosinusului.cpp -o teorema_cosinusului
+g++ -O0 -pipe cmmdc.cpp -o cmmdc &
+g++ -O0 -pipe divizori.cpp -o divizori &
+g++ -O0 -pipe extragerea_radicalilor.cpp -o extragerea_radicalilor &
+g++ -O0 -pipe heron.cpp -o heron &
+g++ -O0 -pipe simplif_fractii.cpp -o simplif_fractii &
+g++ -O0 -pipe prime_factors.cpp -o prime_factors &
+g++ -O0 -pipe teorema_cosinusului.cpp -o teorema_cosinusului &
+
+wait
